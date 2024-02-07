efb728892ded23a220768e5cf11521cb8ff9f07b
Taiki <aikitaiki@icloud.com>
2024-02-07 19:31:06 +0900

    名前をたいきに、自己紹介のため多くの変更を行った

--- HalTestClient/Source/Data.h
+++ HalTestClient/Source/Data.h
@@ -1,6 +1,6 @@
-namespace Hoshiyama
+namespace Taiki
 {
-	const char* TextMessage1 = "私の名前は三郎です。";
-	const char* TextMessage2 = "私の年齢は20歳です。";
-	const char* TextMessage3 = "私の好きな食べ物は焼肉です。";
+	const char* TextMessage1 = "私の名前は大塚大喜です。";
+	const char* TextMessage2 = "私の年齢は23歳です。";
+	const char* TextMessage3 = "私の好きな食べ物はカレーです。";
 }
